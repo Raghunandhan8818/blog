c304aff1f52c06d282be43a1f981926a2f2c99eb
raghunandhan <raghunandhan8818@gmail.com>
2021-04-12 10:27:10 +0530

    fourth commit environ

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -160,5 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'raghunandhan8818@gmail.com'
-EMAIL_HOST_PASSWORD = 'kan2kasby2021'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+print(EMAIL_HOST_USER)
+print(EMAIL_HOST_PASSWORD)
